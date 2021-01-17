04a2e2694b641ecfbde6cacf002581943b551eb0
annkit24 <ankit.21810509@viit.ac.in>
2021-01-17 16:39:18 +0530

    Birthday greeting

---
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.birthdaycard
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun createActivity(view: View) {
+        val name = nameInput.editableText.toString()
+    //    Toast.makeText(this,"Button Was Clicked",Toast.LENGTH_LONG).show()
+        val intent = Intent(this,MainActivity2::class.java)
+        intent.putExtra(MainActivity2.NAME_EXTRA,name)
+        startActivity(intent)
+    }
+}
